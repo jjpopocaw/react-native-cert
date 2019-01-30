a87ee0d2910270847663d337a5a515b896ea3c46
jjpopocaw <joaquin.popoca@MacBook-Jaquin-Popoca.local>
2019-01-30 16:04:19 -0600

    Chaange some card details.

--- Components/Tarjeta.js
+++ Components/Tarjeta.js
@@ -8,12 +8,20 @@ export default class Tarjeta extends React.Component {
     }
 
     render() {
-        let messageToShow = "";
+        // Trim message
+        let messageToShow = this.props.question;
         if (this.props.info.question.length > 90) {
             messageToShow = this.props.info.question.substring(0, 90) + '...';
         }
-        else {
-            messageToShow = this.props.question;
+        // Calculate dates
+        const createDate = new Date(this.props.info.createdAt);
+        const today = new Date();
+        const timeDiff = Math.abs(createDate.getTime() - today.getTime());
+        const dayDiff = Math.ceil(timeDiff / (1000 * 3600 *24));
+        // Get name
+        let name = "Anonymous";
+        if (!this.props.info.is_anonymous) {
+            name = this.props.info.created_by_user.full_name;
         }
         return(
             <TouchableOpacity
@@ -21,16 +29,16 @@ export default class Tarjeta extends React.Component {
                 onPress={ () => this.props.navigation.navigate('Detalle', {info: this.props.info}) }
             >
                 <View style={styles.header}>
-                    <Text>Anonymous</Text>
-                    <Text>7 days</Text>
+                    <Text>{name}</Text>
+                    <Text>{dayDiff} days ago</Text>
                     <Text>Q: {this.props.info.question_id}</Text>
                 </View>
                 <View style={styles.content}>
                     <Text style={styles.base}>{messageToShow}</Text>
                 </View>
                 <View style={styles.footer}>
-                    <Text>{this.props.info.Votes}</Text>
-                    <Text>{this.props.info.numComments}</Text>
+                    <Text style={styles.footerElement}>Likes: {this.props.info.Votes}</Text>
+                    <Text style={styles.footerElement}>Comments: {this.props.info.numComments}</Text>
                 </View>
             </TouchableOpacity>
         );
@@ -62,5 +70,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'flex-end',
+    },
+    footerElement: {
+        margin: 3,
     }
 })
